46f0cb4707cdd478f17429c77551a4856401555d
cc-djy <836633504@qq.com>
2019-07-27 22:10:38 +0800

    ssm整合中，没解决问题：Invalid bound statement (not found): com.ssm.mapper.ManagerMapper.login

--- springMvcLearning/src/main/Java/com/ssm/controller/UserController2.java
+++ springMvcLearning/src/main/Java/com/ssm/controller/UserController2.java
@@ -1,6 +1,8 @@
 package com.ssm.controller;
 
 import com.ssm.pojo.Manager;
+import com.ssm.service.IManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,11 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserController2 {
-
+    @Autowired
+    private IManagerService managerService;
     @RequestMapping("/get")
     @ResponseBody
-    public Manager get(){
-        return new Manager();
+    public Manager get() throws Exception {
+        System.out.println("come in");
+        Manager manager = managerService.loginService("cc");
+        System.out.println(manager);
+        return manager;
     }
+
+
 
 }
--- springMvcLearning/src/main/Java/com/ssm/pojo/Manager.java
+++ springMvcLearning/src/main/Java/com/ssm/pojo/Manager.java
@@ -5,19 +5,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class Manager {
     private String name;
+    private String password;
+    private String gender;
+    private Integer age;
 
     public Manager() {
     }
 
-    public Manager(String name) {
+    public Manager(String name, String password, String gender, Integer age) {
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Manager{" +
-                "name='" + name + '\'' +
-                '}';
+        this.password = password;
+        this.gender = gender;
+        this.age = age;
     }
 
     public String getName() {
@@ -26,5 +25,39 @@ public class Manager {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
